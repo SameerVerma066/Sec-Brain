9cd4265ae16dbc53800a4394da5b3f2f01d7a163
SameerVerma066@ <SameerVerma066@>
2026-03-28 09:12:19 +0000

    guessing Turbopack + Tailwind 4 compatibility is causing some issue so moving to plain css for a while

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveAlias: {
+      tailwindcss: "tailwindcss",
+    },
+  },
 };
 
 export default nextConfig;
